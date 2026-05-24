d4b97ce4834a83abe70330bdafea6dbaa97b6cf7
juncheng <working_fang@163.com>
2026-05-24 08:32:11 +0000

    feat: add integer-to-float conversion traits

--- src/traits/sealed.rs
+++ src/traits/sealed.rs
@@ -1,17 +1,33 @@
-pub trait Int {}
-pub trait Unsigned {}
+pub trait Int: Copy {
+    fn as_f32(self) -> f32;
+    fn as_f64(self) -> f64;
+}
+
+pub trait Unsigned: Int {}
 
 macro_rules! impl_int {
-        ($($ty:ty),* $(,)?) => {
-            $(impl Int for $ty {})*
-        };
-    }
+    ($($ty:ty),* $(,)?) => {
+        $(
+            impl Int for $ty {
+                #[inline]
+                fn as_f32(self) -> f32 {
+                    self as f32
+                }
+
+                #[inline]
+                fn as_f64(self) -> f64 {
+                    self as f64
+                }
+            }
+        )*
+    };
+}
 
 macro_rules! impl_unsigned {
-        ($($ty:ty),* $(,)?) => {
-            $(impl Unsigned for $ty {})*
-        };
-    }
+    ($($ty:ty),* $(,)?) => {
+        $(impl Unsigned for $ty {})*
+    };
+}
 
 impl_int!(
     i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize,
